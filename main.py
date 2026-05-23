454475b1f1a00522c590a42bd7dca58538b9ab5b
barraxbsi <wagnerbarraldaconceicao@gmail.com>
2026-05-23 15:00:57 -0300

    Aplica DIP: dependências injetadas; código agora é testável

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# main: executar interface CLI do sistema.
-
 from services.servico_emprestimo import ServicoEmprestimo
-
+from repositories.repositorio_emprestimo import RepositorioEmprestimo
+from notifications.notificador import Notificador
 
 def menu():
     print("\n1 - Registrar empréstimo")
@@ -9,52 +8,37 @@ def menu():
     print("3 - Listar atrasados")
     print("4 - Sair")
 
-
 def main():
-    servico = ServicoEmprestimo()
+    repositorio = RepositorioEmprestimo()
+    notificador = Notificador()
+    servico = ServicoEmprestimo(repositorio, notificador)
 
     while True:
         menu()
-
         opcao = input("Escolha: ")
 
         if opcao == "1":
             equipamento = input("Equipamento: ")
             usuario = input("Usuário: ")
-
-            servico.registrar_emprestimo(
-                equipamento,
-                usuario
-            )
+            servico.registrar_emprestimo(equipamento, usuario)
 
         elif opcao == "2":
             equipamento = input("Equipamento: ")
             dias = int(input("Dias de atraso: "))
-
-            servico.devolver_equipamento(
-                equipamento,
-                dias
-            )
+            servico.devolver_equipamento(equipamento, dias)
 
         elif opcao == "3":
             atrasados = servico.listar_atrasados()
-
             if not atrasados:
                 print("Nenhum empréstimo atrasado.")
-
             for emprestimo in atrasados:
-                print(
-                    emprestimo["usuario"],
-                    "-",
-                    emprestimo["equipamento"]["nome"]
-                )
+                print(emprestimo["usuario"], "-", emprestimo["equipamento"]["nome"])
 
         elif opcao == "4":
             break
 
         else:
             print("Opção inválida.")
-
 
 if __name__ == "__main__":
     main()
