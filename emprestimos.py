3675c904542ec0bc5d41f76fd7361f4aa4da4896
barraxbsi <wagnerbarraldaconceicao@gmail.com>
2026-06-09 19:34:18 -0300

    refactor: extrai regra de limite

--- emprestimos.py
+++ emprestimos.py
@@ -1,3 +1,9 @@
 
 def pode_realizar_emprestimo(emprestimos_abertos, limite):
     return emprestimos_abertos < limite
+def atingiu_limite(emprestimos_abertos, limite):
+    return emprestimos_abertos >= limite
+
+
+def pode_realizar_emprestimo(emprestimos_abertos, limite):
+    return not atingiu_limite(emprestimos_abertos, limite)
--- tests/test_emprestimos.py
+++ tests/test_emprestimos.py
@@ -1,10 +1,7 @@
 from emprestimos import pode_realizar_emprestimo
 
-
 def test_permite_emprestimo_quando_abaixo_do_limite():
-    assert pode_realizar_emprestimo(
-        emprestimos_abertos=2,
-        limite=3
-    ) is True
-    def test_bloqueia_quando_atinge_limite():
+    assert pode_realizar_emprestimo(2, 3) is True
+
+def test_bloqueia_quando_atinge_limite():
     assert pode_realizar_emprestimo(3, 3) is False
